5ae5cb1ee84d5d5b38a96555d40ab21e32c68a9f
Joshua Edo <joshua.edo01@gmail.com>
2022-10-19 14:02:11 +0000

    render pieces on board

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,14 +1,24 @@
 import "./Tile.css";
 
 interface Props {
-    number: number;
+  image?: string;
+  number: number;
 }
 
-export default function Tile({ number }: Props) {
+// "assets/images/pawn_b.png"
 
-    if (number % 2 === 0) {
-        return <div className="tile black-tile"></div>;
-    } else {
-        return <div className="tile white-tile"></div>;
-    }
+export default function Tile({ number, image }: Props) {
+  if (number % 2 === 0) {
+    return (
+      <div className="tile black-tile">
+        <img src={image} alt="" />
+      </div>
+    );
+  } else {
+    return (
+      <div className="tile white-tile">
+        <img src={image} alt="" />
+      </div>
+    );
+  }
 }
