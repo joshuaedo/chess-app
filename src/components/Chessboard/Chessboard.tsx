27761b48140b68c31ab426867469e883c5011884
Joshua Edo <joshua.edo01@gmail.com>
2022-10-19 16:02:58 +0000

    add chess pieces and make them movable

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import Tile from "../Tile/Tile";
 import "./Chessboard.css";
 
@@ -33,6 +33,37 @@ for (let p = 0; p < 2; p++) {
   pieces.push({ image: `assets/images/rook_${type}.png`, x: 7, y });
 }
 
+let activePiece: HTMLElement | null = null;
+
+function grabPiece(e: React.MouseEvent) {
+  const element = e.target as HTMLElement;
+  if (element.classList.contains("chess-piece")) {
+    const x = e.clientX - 50;
+    const y = e.clientY - 50;
+    element.style.position = "absolute";
+    element.style.left = `${x}px`;
+    element.style.top = `${y}px`;
+
+    activePiece = element;
+  }
+}
+
+function movePiece(e: React.MouseEvent) {
+  if (activePiece) {
+    const x = e.clientX - 50;
+    const y = e.clientY - 50;
+    activePiece.style.position = "absolute";
+    activePiece.style.left = `${x}px`;
+    activePiece.style.top = `${y}px`;
+  }
+}
+
+function dropPiece(e: React.MouseEvent) {
+  if (activePiece) {
+    activePiece = null;
+  }
+}
+
 export default function Chessboard() {
   let board = [];
 
@@ -49,9 +80,18 @@ export default function Chessboard() {
         }
       });
 
-      board.push(<Tile image={image} number={number} />);
+      board.push(<Tile key={`${i},${j}`} image={image} number={number} />);
     }
   }
 
-  return <div id="chessboard">{board}</div>;
+  return (
+    <div
+      onMouseDown={(e) => grabPiece(e)}
+      onMouseMove={(e) => movePiece(e)}
+      onMouseUp={(e) => dropPiece(e)}
+      id="chessboard"
+    >
+      {board}
+    </div>
+  );
 }
